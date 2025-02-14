182fa6ecfa18601d68607b73548b67cef2526df2
Jan Fincke <jan.fincke@iki.fi>
2025-02-14 22:59:55 +0200

    First commit

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode, TextType
+
+
+if __name__ == '__main__':
+    text_node = TextNode("asd", TextType.TEXT)
+    print(text_node)
---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = 1
+    BOLD = 2
+    ITALIC = 3
+    UNDERLINE = 4
+    STRIKETHROUGH = 5
+    CODE = 6
+    LINK = 7
+
+class TextNode:
+    def __init__(self, text: str, TextType: TextType, url: str = None):
+        self.text = text
+        self.text_type = TextType
+        self.url = None
+    
+    def __eq__(self, value):
+        return self.text == value.text and self.text_type.value == value.text_type.value and self.url == value.url
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
